f7d3e3fb71ac47c7cc5545484a0be16b67ace1d3
mcpar-land <john@mcpar.land>
2021-12-30 13:29:35 -0600

    work

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,63 @@ use crate::screen::Screen;
 
 mod screen;
 
-const SCREEN_WIDTH: usize = 80;
-const SCREEN_HEIGHT: usize = 40;
-const FONT_SIZE: u16 = 18;
+pub const SCREEN_WIDTH: usize = 80;
+pub const SCREEN_HEIGHT: usize = 40;
+pub const FONT_SIZE: u16 = 12;
+pub const PADDING: u16 = 10;
+pub const FONT_RATIO: f32 = 1.5;
 
-#[macroquad::main("Term Game Lib")]
+fn window_conf() -> Conf {
+	let padding = (PADDING * 2) as i32;
+	let h = (SCREEN_HEIGHT as f32 * FONT_SIZE as f32 * FONT_RATIO).floor() as i32;
+	Conf {
+		window_title: "Term Game Lib".to_string(),
+		window_width: SCREEN_WIDTH as i32 * FONT_SIZE as i32 + padding,
+		window_height: h + padding,
+		window_resizable: false,
+		high_dpi: true,
+		..Default::default()
+	}
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
 	let font = load_ttf_font_from_bytes(include_bytes!(
 		"../resources/CascadiaCode-Regular.ttf"
 	))
 	.unwrap();
-	let mut screen = Screen::<SCREEN_WIDTH, SCREEN_HEIGHT>::new();
+	let mut screen =
+		Screen::<SCREEN_WIDTH, SCREEN_HEIGHT>::new(PADDING as f32, PADDING as f32);
 
+	let mut picker = 0;
+	let one_at_a_time = "BINGUSMYBELOVED";
+	for x in 0..SCREEN_WIDTH {
+		for y in 0..SCREEN_HEIGHT {
+			screen.write((x, y), ":", GRAY);
+		}
+	}
 	loop {
 		clear_background(BLACK);
+		screen.write((0, 0), "0", RED);
+		screen.write((79, 39), "X", RED);
+
 		screen.write((8, 4), "Hello, world!", GREEN);
-		screen.draw(FONT_SIZE, font);
-		draw_text(&get_fps().to_string(), 10.0, 10.0, 8.0, WHITE);
+		screen.write(
+			(13, 5),
+			&format!("{}", one_at_a_time.chars().nth(picker).unwrap()),
+			YELLOW,
+		);
+		screen.write((20, 20), "This one has\na newline in it", BLUE);
+		let draws = screen.draw(FONT_SIZE, font);
+		let debug = format!("{} - glyphs: {}", get_fps(), draws);
+		draw_text(&debug, 10.0, 10.0, 8.0, WHITE);
+
+		if picker >= one_at_a_time.len() - 1 {
+			picker = 0;
+		} else {
+			picker += 1;
+		}
+
 		next_frame().await;
 	}
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,3 +1,4 @@
+use crate::{FONT_RATIO, PADDING};
 use std::os::windows::prelude::OsStrExt;
 
 use macroquad::{
@@ -11,59 +12,76 @@ pub struct Glyph {
 	pub color: Color,
 }
 
-pub struct Screen<const W: usize, const H: usize>(pub [[Glyph; W]; H]);
+pub struct Screen<const W: usize, const H: usize> {
+	pub glyphs: [[Glyph; W]; H],
+	pub x: f32,
+	pub y: f32,
+}
 
 impl<const W: usize, const H: usize> Screen<W, H> {
-	pub fn new() -> Self {
-		Self(
-			[[Glyph {
+	pub fn new(x: f32, y: f32) -> Self {
+		Self {
+			glyphs: [[Glyph {
 				ch: ' ',
 				color: macroquad::color::WHITE,
 			}; W]; H],
-		)
+			x,
+			y,
+		}
 	}
 
 	pub fn write(&mut self, (x, y): (usize, usize), s: &str, color: Color) {
 		if y >= H {
 			return;
 		}
+		let mut progress_x = 0;
+		let mut y = y;
 		for (i, c) in s.chars().enumerate() {
-			if x + i >= W {
+			if x + i >= W || y >= H {
 				return;
 			}
-			self.0[y][x + i] = Glyph { ch: c, color };
+			if c == '\n' {
+				y += 1;
+				progress_x = 0;
+			} else {
+				self.glyphs[y][x + progress_x] = Glyph { ch: c, color };
+				progress_x += 1;
+			}
 		}
 	}
 
 	pub fn clear(&mut self) {
-		self.0 = [[Glyph {
+		self.glyphs = [[Glyph {
 			ch: ' ',
 			color: macroquad::color::WHITE,
 		}; W]; H]
 	}
 
-	pub fn draw(&self, font_size: u16, font: Font) {
+	pub fn draw(&self, font_size: u16, font: Font) -> usize {
+		let mut draws_performed: usize = 0;
 		for y in 0..H {
 			for x in 0..W {
-				let glyph = &self.0[y][x];
+				let glyph = &self.glyphs[y][x];
 				let mut tmp = [0u8; 4];
 				let s = glyph.ch.encode_utf8(&mut tmp);
 
 				if glyph.ch != ' ' {
 					draw_text_ex(
 						s,
-						x as f32 * font_size as f32,
-						y as f32 * font_size as f32,
+						x as f32 * font_size as f32 + self.x,
+						(y + 1) as f32 * font_size as f32 * FONT_RATIO + self.y,
 						TextParams {
 							font,
 							font_size,
 							font_scale: 1.0,
 							font_scale_aspect: 1.0,
-							color: self.0[y][x].color,
+							color: self.glyphs[y][x].color,
 						},
 					);
+					draws_performed += 1;
 				}
 			}
 		}
+		draws_performed
 	}
 }
